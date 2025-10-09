84ef3c1da902f62dcbb48714c457f752bc695332
laveeza-h <laveeza46@gmail.com>
2025-10-09 18:27:29 +0000

    change

---
+++ java/heapandstack.js
@@ -0,0 +1,8 @@
+let name = "Laveeza"; // Primitive (stored in stack)
+let age = 21;         // Primitive (stored in stack)
+
+function greet() {
+  let message = "Hello!"; // Local variable (in stack)
+  console.log(message);
+}
+greet();
