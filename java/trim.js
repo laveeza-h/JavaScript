797d5e37a72ce110775973fe9049a54f87216493
laveeza-h <laveeza46@gmail.com>
2025-10-10 11:13:14 +0000

    change

---
+++ java/trim.js
@@ -0,0 +1,11 @@
+let text = "   Hello JavaScript!   ";
+let result = text.trim();
+
+console.log(result);        // "Hello JavaScript!"
+console.log(text.length);   // 22 (includes spaces)
+console.log(result.length); // 17 (spaces removed)
+let text1 = "I love Python";
+let result1 = text.replace("Python", "JavaScript");
+
+console.log(result1); // "I love JavaScript"
+
