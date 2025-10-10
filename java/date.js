483584004102c687ac9d7579ca3758a682d742ed
laveeza-h <laveeza46@gmail.com>
2025-10-10 16:55:26 +0000

    change

---
+++ java/date.js
@@ -0,0 +1,11 @@
+let now = new Date();
+
+console.log("Year:", now.getFullYear());
+console.log("Month:", now.getMonth() + 1); // +1 because 0 = Jan
+console.log("Date:", now.getDate());
+console.log("Day:", now.getDay());
+console.log("Hour:", now.getHours());
+console.log("Minutes:", now.getMinutes());
+console.log("Seconds:", now.getSeconds());
+console.log("Milliseconds:", now.getMilliseconds());
+console.log("Timestamp:", now.getTime());
