b41dd533b8875d62eeb8a9fa8fc45d2426aba7a6
laveeza-h <laveeza46@gmail.com>
2025-10-10 12:31:29 +0000

    change

---
+++ java/numbers.js
@@ -0,0 +1,8 @@
+let num = 123.456;
+
+console.log(num.toFixed(2));       // "123.46"
+console.log(num.toPrecision(4));   // "123.5"
+console.log(parseInt("25px"));     // 25
+console.log(parseFloat("25.75kg"));// 25.75
+console.log(Number("123"));        // 123
+console.log(isNaN("Hello"));       // true
