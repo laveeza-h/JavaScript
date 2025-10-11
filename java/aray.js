cc6a0daa01f0884240447d124f82dc1de2fe44d7
laveeza-h <laveeza46@gmail.com>
2025-10-11 18:12:22 +0000

    change

---
+++ java/aray.js
@@ -0,0 +1,4 @@
+let animals = ["Cat", "Dog", "Cow"];
+console.log(animals[1]);  // "Dog"
+animals[1] = "Goat";      // Modify element
+console.log(animals);     // ["Cat", "Goat", "Cow"]
---
+++ java/aray1.js
@@ -0,0 +1,10 @@
+let numbers = [10, 5, 20, 1];
+numbers.sort((a, b) => a - b); // ascending
+console.log(numbers); // [1, 5, 10, 20]
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+console.log(matrix[1][2]); // 6
